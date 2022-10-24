d8b957732e55637ce18cab408058c86b6705eb5e
Rose <roza.nussipbek@gmail.com>
2022-10-24 11:17:37 -0400

    Initial commit

--- src/test/java/interview/test/Interview.java
+++ src/test/java/interview/test/Interview.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class Interview {
     public static void main(String[] args) {
 
+        System.out.println("Test");
+
+        System.out.println("hello");
     }
 
 
